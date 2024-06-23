e745e7b0837fb885d2fa04df454686fd176c2bd8
TajalTechnology <mrpresident1189@gmail.com>
2024-06-23 22:44:44 +0600

    refactor

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { AuthProvider } from './context/AuthContext';
 import ProtectedRoute from './components/ProtectedRoute';
 import Login from './components/Login';
 import Register from './components/Register';
